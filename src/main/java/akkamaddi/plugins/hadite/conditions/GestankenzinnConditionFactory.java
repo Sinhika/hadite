113a6d3f2e4b56f8dea1ce5163a3f82c8a55537e
Sinhika <sinhika@republicofnewhome.org>
2018-10-30 08:16:34 -0500

    filled in Gestankenzinn factory

--- src/main/java/akkamaddi/plugins/hadite/conditions/GestankenzinnConditionFactory.java
+++ src/main/java/akkamaddi/plugins/hadite/conditions/GestankenzinnConditionFactory.java
@@ -4,6 +4,9 @@ import java.util.function.BooleanSupplier;
 
 import com.google.gson.JsonObject;
 
+import akkamaddi.plugins.hadite.ModInfo;
+import akkamaddi.plugins.hadite.Settings;
+import net.minecraft.util.JsonUtils;
 import net.minecraftforge.common.crafting.IConditionFactory;
 import net.minecraftforge.common.crafting.JsonContext;
 
@@ -13,7 +16,19 @@ public class GestankenzinnConditionFactory implements IConditionFactory
 	@Override
 	public BooleanSupplier parse(JsonContext context, JsonObject json) 
 	{
-		// TODO Auto-generated method stub
+		boolean value = JsonUtils.getBoolean(json , "value", true);
+		String key = JsonUtils.getString(json, "type");
+		
+		boolean metal_enabled = Settings.gestankenzinnBlock.isEnabled() 
+				&& Settings.gestankenzinnIngot.isEnabled();
+		
+		// at all
+		if (key.equals(ModInfo.ID + ":gestankenzinn_enabled")) {
+			return () ->metal_enabled == value;
+		}
+		if (key.equals(ModInfo.ID + ":gestankenzinn_tools_enabled")) {
+			return () -> Settings.gestankenzinnTools.isEnabled() == value;
+		}
 		return null;
 	}
 
