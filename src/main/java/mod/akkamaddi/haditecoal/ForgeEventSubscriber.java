6ec093ef5b698e3b232ee496040f2d6eabc0f1a2
Sinhika <sinhika@republicofnewhome.org>
2021-04-27 17:35:16 -0500

    added injection loot table lookup class (skeletal), and LootLoading event handler to ForgeEventSubscriber

--- src/main/java/mod/akkamaddi/haditecoal/ForgeEventSubscriber.java
+++ src/main/java/mod/akkamaddi/haditecoal/ForgeEventSubscriber.java
@@ -3,8 +3,12 @@ package mod.akkamaddi.haditecoal;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import mod.akkamaddi.haditecoal.config.HaditeConfig;
 import mod.akkamaddi.haditecoal.generation.OreGeneration;
+import mod.akkamaddi.haditecoal.loot.HaditeInjectionLookup;
+import mod.alexndr.simplecorelib.helpers.LootUtils;
 import net.minecraft.world.biome.Biome;
+import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -15,6 +19,21 @@ public final class ForgeEventSubscriber
 {
     @SuppressWarnings("unused")
     private static final Logger LOGGER = LogManager.getLogger(HaditeCoal.MODID + " Forge Event Subscriber");
+    private static final HaditeInjectionLookup lootLookupMap = new HaditeInjectionLookup();
+    
+    /**
+     * add mod loot to loot tables. Code heavily based on Botania's LootHandler, which
+     * neatly solves the problem when I couldn't figure it out.
+     */
+    @SubscribeEvent
+    public static void LootLoad(final LootTableLoadEvent event)
+    {
+        if (HaditeConfig.addChestLoot)
+        {
+            LootUtils.LootLoadHandler(HaditeCoal.MODID, event, lootLookupMap);
+        } // end-if config allows
+    } // end LootLoad()
+    
     
     /**
      * Biome loading triggers ore generation.
---
+++ src/main/java/mod/akkamaddi/haditecoal/loot/HaditeInjectionLookup.java
@@ -0,0 +1,14 @@
+package mod.akkamaddi.haditecoal.loot;
+
+import mod.alexndr.simplecorelib.helpers.InjectionTableLookup;
+
+@SuppressWarnings("serial")
+public class HaditeInjectionLookup extends InjectionTableLookup
+{
+
+    public HaditeInjectionLookup()
+    {
+        super();
+    }
+
+} // end class
